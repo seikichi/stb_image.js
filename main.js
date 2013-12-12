5765cbb2b049766ab11a1f0cdf121e3bfc6a507d
seikichi <seikichi@kmc.gr.jp>
2013-12-12 12:49:11 +0900

    shows error if the result data is empty

--- main.js
+++ main.js
@@ -1,37 +1,42 @@
-
-
 function showImage(file) {
   var url = (window.URL || window.webkitURL).createObjectURL(file);
   var worker = new Worker('./main.worker.js');
   worker.onmessage = function (event) {
     var image = event.data;
-
-    var canvas = document.createElement('canvas');
-    canvas.width = image.width;
-    canvas.height = image.height;
-    var context = canvas.getContext("2d");
-    var imageData = context.createImageData(image.width, image.height);
-    var source = image.data;
-    var dest = imageData.data;
-    if ('set' in dest) {
-      dest.set(source);
+    if (image.data.length === 0) {
+      showError('Invalid Image');
     } else {
-      for (var i = 0, len = dest.length; i < len; ++i) {
-        dest[i] = source[i];
+      var canvas = document.createElement('canvas');
+      canvas.width = image.width;
+      canvas.height = image.height;
+      var context = canvas.getContext("2d");
+      var imageData = context.createImageData(image.width, image.height);
+      var source = image.data;
+      var dest = imageData.data;
+      if ('set' in dest) {
+        dest.set(source);
+      } else {
+        for (var i = 0, len = dest.length; i < len; ++i) {
+          dest[i] = source[i];
+        }
       }
+      context.putImageData(imageData, 0, 0);
+      $('#output').html(canvas);
     }
-    context.putImageData(imageData, 0, 0);
-    $('#output').html(canvas);
 
     worker.terminate();
     worker = null;
   };
   worker.onerror = function (error) {
-    console.log(error);
+    showError(error);
     worker.teminate();
     worker = null;
   };
   worker.postMessage({url: url});
+}
+
+function showError(message) {
+  alert(message);
 }
 
 $('#file').on('change', function (event) {
