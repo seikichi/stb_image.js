bc3bfa2cae8fd7ad52d69a362d239b3b6a4bd440
seikichi <seikichi@kmc.gr.jp>
2013-12-14 12:30:28 +0900

    wraps the generated code by '(function(){%s})();'

--- build.sh
+++ build.sh
@@ -7,7 +7,10 @@ wget http://nothings.org/stb_image.c
 
 tsc --noImplicitAny export.ts
 
-emcc stb_image.c -o dist/stb_image.js \
-    --pre-js <(cat <(echo "(function() {") pre.js) \
-    --post-js <(cat post.js export.js <(echo "})();")) \
+emcc stb_image.c -o a.out.js \
+    --pre-js pre.js \
+    --post-js <(cat post.js export.js) \
     -s EXPORTED_FUNCTIONS="['_stbi_load','_stbi_image_free','FS']"
+
+cat <(echo "(function(){") a.out.js  <(echo "})();") > dist/stb_image.js
+rm -rf a.out.js
