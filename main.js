656796938e056eeec7625c1a6f978059ae42dca8
seikichi <seikichi@kmc.gr.jp>
2013-12-14 11:53:39 +0900

    updates

--- main.js
+++ main.js
@@ -1,37 +1,32 @@
+var worker = new Worker('./main.worker.js');
+
+worker.onmessage = function (event) {
+  var image = event.data.image;
+  var diff = event.data.diff;
+  $('#time').html(diff);
+  var canvas = document.createElement('canvas');
+  canvas.width = image.width;
+  canvas.height = image.height;
+  var context = canvas.getContext("2d");
+  var imageData = context.createImageData(image.width, image.height);
+  var source = image.data;
+  var dest = imageData.data;
+  if ('set' in dest) {
+    dest.set(source);
+  } else {
+    for (var i = 0, len = dest.length; i < len; ++i) {
+      dest[i] = source[i];
+    }
+  }
+  context.putImageData(imageData, 0, 0);
+  $('#output').html(canvas);
+};
+worker.onerror = function (error) {
+  showError(error);
+};
+
 function showImage(file) {
   var url = (window.URL || window.webkitURL).createObjectURL(file);
-  var worker = new Worker('./main.worker.js');
-  worker.onmessage = function (event) {
-    var image = event.data;
-    if (image.data.length === 0) {
-      showError('Invalid Image');
-    } else {
-      var canvas = document.createElement('canvas');
-      canvas.width = image.width;
-      canvas.height = image.height;
-      var context = canvas.getContext("2d");
-      var imageData = context.createImageData(image.width, image.height);
-      var source = image.data;
-      var dest = imageData.data;
-      if ('set' in dest) {
-        dest.set(source);
-      } else {
-        for (var i = 0, len = dest.length; i < len; ++i) {
-          dest[i] = source[i];
-        }
-      }
-      context.putImageData(imageData, 0, 0);
-      $('#output').html(canvas);
-    }
-
-    worker.terminate();
-    worker = null;
-  };
-  worker.onerror = function (error) {
-    showError(error);
-    worker.teminate();
-    worker = null;
-  };
   worker.postMessage({url: url});
 }
 
--- main.worker.js
+++ main.worker.js
@@ -1,19 +1,27 @@
 importScripts('stb_image.js');
 
+STBImage.initialize({
+  TOTAL_MEMORY: 16777216 * 8
+});
+
 self.onmessage = function (event) {
   var url = event.data.url;
+  var start = Date.now();
   var xhr = new XMLHttpRequest();
   xhr.open('GET', url, false);
   xhr.responseType = 'arraybuffer';
   xhr.send(null);
 
   var buffer = xhr.response;
-  var image = new STBImage(buffer, {
-    TOTAL_MEMORY: 16777216 * 8
-  });
+  var image = new STBImage(buffer);
+  var end = Date.now();
   self.postMessage({
-    data: image.data,
-    width: image.width,
-    height: image.height,
+    diff: end - start,
+    image: {
+      data: image.data,
+      width: image.width,
+      height: image.height,
+    }
   });
+  image.close();
 };
